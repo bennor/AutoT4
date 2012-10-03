ec3081626ded705bfcba674ecf119bffa1cfe7c0
Bennor McCarthy <bennor@gmail.com>
2012-10-04 07:51:52 +1000

    Initial commit
    Copied most of the code from AutoT4MVC and modified it to be more
    generic.

---
+++ AutoT4/AutoT4Package.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using EnvDTE;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BennorMcCarthy.AutoT4
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [Guid(GuidList.guidAutoT4PkgString)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string)]
+    public sealed class AutoT4Package : Package
+    {
+        private DTE dte;
+        private BuildEvents buildEvents;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            dte = GetService(typeof(SDTE)) as DTE;
+
+            buildEvents = dte.Events.BuildEvents;
+            buildEvents.OnBuildBegin += OnBuildBegin;
+        }
+
+        private void RunTemplates(params Project[] projects)
+        {
+            if (projects == null)
+                return;
+
+            var templates = FindProjectItems(@"\.[Tt][Tt]$", projects).ToList();
+            foreach (var template in templates)
+            {
+                if (!template.IsOpen)
+                    template.Open();
+                template.Save();
+            }
+        }
+
+        private void OnBuildBegin(vsBuildScope Scope, vsBuildAction Action)
+        {
+            IEnumerable<Project> projects = null;
+            switch (Scope)
+            {
+                case vsBuildScope.vsBuildScopeSolution:
+                    projects = dte.Solution.Projects.OfType<Project>();
+                    break;
+                case vsBuildScope.vsBuildScopeProject:
+                    projects = ((object[])dte.ActiveSolutionProjects).OfType<Project>();
+                    break;
+                default:
+                    return;
+            }
+
+            RunTemplates(projects.ToArray());
+        }
+
+        private IEnumerable<ProjectItem> FindProjectItems(string pattern, IEnumerable<Project> projects)
+        {
+            if (projects == null)
+                projects = dte.Solution.Projects.OfType<Project>();
+
+            var regex = new Regex(pattern);
+            foreach (Project project in projects)
+            {
+                foreach (var projectItem in FindProjectItems(regex, project.ProjectItems))
+                    yield return projectItem;
+            }
+        }
+
+        private static IEnumerable<ProjectItem> FindProjectItems(Regex regex, ProjectItems projectItems)
+        {
+            foreach (ProjectItem projectItem in projectItems)
+            {
+                if (regex.IsMatch(projectItem.Name ?? ""))
+                    yield return projectItem;
+
+                if (projectItem.ProjectItems != null)
+                {
+                    foreach (var subItem in FindProjectItems(regex, projectItem.ProjectItems))
+                        yield return subItem;
+                }
+                if (projectItem.SubProject != null)
+                {
+                    foreach (var subItem in FindProjectItems(regex, projectItem.SubProject.ProjectItems))
+                        yield return subItem;
+                }
+            }
+        }
+
+        protected override int QueryClose(out bool canClose)
+        {
+            int result = base.QueryClose(out canClose);
+            if (!canClose)
+                return result;
+
+            if (buildEvents != null)
+            {
+                buildEvents.OnBuildBegin -= OnBuildBegin;
+                buildEvents = null;
+            }
+            return result;
+        }
+    }
+}
---
+++ AutoT4/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace BennorMcCarthy.AutoT4
+{
+    static class GuidList
+    {
+        public const string guidAutoT4PkgString = "3fe81b73-39c9-4e39-8742-8e3fc01493bd";
+        public const string guidAutoT4CmdSetString = "1b1b018a-07fd-4501-bac2-55a65d4971fe";
+
+        public static readonly Guid guidAutoT4CmdSet = new Guid(guidAutoT4CmdSetString);
+    };
+}
