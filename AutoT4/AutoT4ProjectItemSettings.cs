955a7bfb68a6d34174909decca7c97f614b16b94
Cosmin Lazar <ccl@d60.dk>
2015-03-14 13:41:27 +0100

    Added the possibility to chose when to run T4 templates (before build, after build, not at all)

--- AutoT4/AutoT4ProjectItemSettings.cs
+++ AutoT4/AutoT4ProjectItemSettings.cs
@@ -11,16 +11,23 @@ namespace BennorMcCarthy.AutoT4
     public class AutoT4ProjectItemSettings : ProjectItemSettings
     {
         public AutoT4ProjectItemSettings(ProjectItem item)
-            : base(item, "AutoT4") {}
+            : base(item, "AutoT4") { }
 
-        [DefaultValue(true)]
+        [DefaultValue(BuildEvent.BeforeBuild)]
         [DisplayName("Run on build")]
         [Category("AutoT4")]
         [Description("Whether to run this template at build time or not.")]
-        public bool RunOnBuild
+        public BuildEvent RunOnBuild
         {
-            get { return Get(true); }
+            get { return Get(BuildEvent.BeforeBuild); }
             set { Set(value); }
         }
+    }
+
+    public enum BuildEvent
+    {
+        DoNotRun,
+        BeforeBuild,
+        AfterBuild
     }
 }
